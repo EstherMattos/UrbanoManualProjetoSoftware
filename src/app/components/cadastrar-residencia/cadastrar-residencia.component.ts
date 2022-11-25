0ca3c38b72e1dde1aeaf2a704b8bc42873f79d8e
EstherMattos <silvaesthermattos@hotmail.com>
2022-11-25 15:28:01 -0300

    com bug nas configurações de usuario

--- src/app/components/cadastrar-residencia/cadastrar-residencia.component.ts
+++ src/app/components/cadastrar-residencia/cadastrar-residencia.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-residencia',
@@ -12,10 +13,12 @@ export class CadastrarResidenciaComponent implements OnInit {
   @Input('enderecos') enderecos: Array<String> = [];
 
   public endereco = new FormControl('');
-
-  constructor() { }
+  public formulario: FormGroup;
+  
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.GeraFormulario();
   }
 
   public EmitirNovoEndereco(): void{
@@ -24,6 +27,14 @@ export class CadastrarResidenciaComponent implements OnInit {
 
     this.emitirNovoEndereco.emit(this.endereco.value)
     this.endereco.patchValue("")
+  }
+
+  public GeraFormulario(): void{
+
+    this.formulario = this._formBuilder.group({
+      enderecoNome: [""]
+    });
+
   }
 
 }
