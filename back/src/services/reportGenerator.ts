8557e8623e3c03567853688ba2f6fd35b4638905
Marcos Novaes <marcosnovaes@id.uff.br>
2022-11-29 08:19:23 -0300

    .

--- back/src/services/reportGenerator.ts
+++ back/src/services/reportGenerator.ts
@@ -1,23 +1,53 @@
 import PDFDocument from 'pdfkit-table';
 import fs from 'fs';
 
-export const generateReport = async (residencia: number, despesas: any[]) => {
-  const doc = new PDFDocument({ margin: 30, size: 'A4' });
-  doc.pipe(fs.createWriteStream('./output.pdf'));
+interface Despesas {
+  id: number;
+  tipo: string;
+  descricao: string;
+  valor: number;
+  residenciaId: number;
+}
 
-  const table = {
-    title: `RELATORIO DE DESPESAS DA RESIDENCIA ${residencia}`,
-    subtitle: 'todo o periodo da residencia',
-    headers: ['ID', 'TIPO', 'DESCRICAO', 'VALOR', 'RESIDENCIA'],
-    rows: [
-      ['1', 'fixa', 'luz', '420', '1'],
-      ['2', 'fixa', 'despesa de luz', '420', '1'],
-    ],
-  };
+class ReportGenerator {
+  residenciaId: number;
+  documentOutputName: string;
+  constructor(residenciaId: number, documentOutputName: string) {
+    this.residenciaId = residenciaId;
+    this.documentOutputName = documentOutputName;
+  }
 
-  await doc.table(table, {
-    columnsSize: [100, 100, 100, 100, 100],
-  });
+  convertItemsToString(data: any): string[][] {
+    return [['a']];
+  }
+}
 
-  doc.end();
-};
+class DespesasReportGenerator extends ReportGenerator {
+  despesas: Despesas;
+  constructor(
+    residenciaId: number,
+    documentOutputName: string,
+    despesas: Despesas
+  ) {
+    super(residenciaId, documentOutputName);
+    this.despesas = despesas;
+  }
+
+  async generateReport() {
+    const doc = new PDFDocument({ margin: 30, size: 'A4' });
+    doc.pipe(fs.createWriteStream(`./${this.documentOutputName}.pdf`));
+
+    const table = {
+      title: `RELATORIO DE DESPESAS DA RESIDENCIA ${this.residenciaId}`,
+      subtitle: 'todo o periodo da residencia',
+      headers: ['ID', 'TIPO', 'DESCRICAO', 'VALOR', 'RESIDENCIA'],
+      rows: this.convertItemsToString(this.despesas),
+    };
+
+    await doc.table(table, {
+      columnsSize: [100, 100, 100, 100, 100],
+    });
+
+    doc.end();
+  }
+}
