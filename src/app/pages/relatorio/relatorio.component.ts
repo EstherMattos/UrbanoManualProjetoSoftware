bd3450880a56377a43ffe0e56d3cff2a9e5c274b
EstherMattos <silvaesthermattos@hotmail.com>
2022-11-28 21:54:35 -0300

    adding css

--- src/app/pages/relatorio/relatorio.component.ts
+++ src/app/pages/relatorio/relatorio.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { InfoUser } from 'src/global/global';
 import { Renda, Residencia, Conta, Produto } from 'src/models/models';
+import { jsPDF } from "jspdf";
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-relatorio',
@@ -12,15 +14,29 @@ import { Renda, Residencia, Conta, Produto } from 'src/models/models';
     public ListaRendas: Array<Renda> = [];
     public ListaContas: Array<Conta> = [];
     public ListaProdutos: Array<Produto> = [];
+
+    @ViewChild('content', {static: false}) el: ElementRef;
+
     constructor() { }
   
     ngOnInit(): void {
+
       this.ListaEnderecos = InfoUser.Usuario.residencias.map(r => r.endereco);
       this.ListaRendas = InfoUser.Usuario.rendas;
       this.ListaContas = InfoUser.Usuario.residencias.map(r => r.contas)[0];
       this.ListaProdutos = InfoUser.Usuario.residencias.map(r => r.estoque.produtos)[0];
-      console.log(this.ListaProdutos);
-      console.log(InfoUser.Usuario);
     }
-    
+    printSimplePDF(){
+      const doc = new jsPDF();
+      doc.text("Hello world!", 10, 10);
+      doc.save("a4.pdf"); 
+    }
+    printPDF(){
+      let pdf = new jsPDF('p', 'pt', 'a4');
+      pdf.html(this.el.nativeElement, {
+        callback: (pdf) => {
+          pdf.save("relatorio.pdf");
+        }
+      })
+    }
   }
